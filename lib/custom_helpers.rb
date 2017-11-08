019a56a00c4f4bb233c496a07bf91be22e0d297f
yana gogo gogo@gogo.tattoo <gogo@gogo.tattoo>
2017-11-08 09:08:47 +0800

    template for team page is too long now.can reuse some table in other pages

--- lib/custom_helpers.rb
+++ lib/custom_helpers.rb
@@ -3,4 +3,15 @@ module CustomHelpers
     images = ["https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/2860a358451835.59fc9745c2e54.jpg", "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/38528c58451835.59fc9745c23fe.jpg", "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/119e5358451835.59fc9745c389e.jpg", "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/ecbe4658451835.59fc9745c2976.jpg"]
     images[rand(images.size)]
   end
+
+  def randomized_tattoo_image
+    images = [
+      "QmSnA3JKxM5u7H5H9VevrMFDwVyQG8SsRUTaY3mnb97Uf7",
+      "QmedCLM2VUFy4cHYWAuUxfkXL5s3LwchLAs6Mk88nvLeDA",
+      "QmYubFp5bwpCPD1g2TtEVbVGuv1GwDekmHH3Fns4J4gJkm",
+      "QmbLNxPnhADeXakfj9KscmBPryVSExd8eaLNaKNDSCDjvQ",
+      "QmatFW7DrLw8BVAqQ3xWf5s2ZQJDCG7bDYpGijXT9vSXPE"
+    ]
+    "https://ipfs.io/ipfs/" + images[rand(images.size)]
+  end
 end
